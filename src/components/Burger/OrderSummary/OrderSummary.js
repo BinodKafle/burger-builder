414e7a7bfc1471049c93f78f6edfa7d0ae459473
Binod Kafle <mebinod50@gmail.com>
2020-01-27 14:55:53 +0545

    Implement button component in order summary modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummery = Object.keys(props.ingredients)
@@ -19,6 +20,8 @@ const orderSummary = (props) => {
                 {ingredientSummery}
             </ul>
             <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
 };
