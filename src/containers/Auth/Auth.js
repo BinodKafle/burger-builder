6360ceaeaf1715205d80740eee6b56eac8faa07e
Binod Kafle <mebinod50@gmail.com>
2020-03-20 18:37:03 +0545

    Redirect user to checkout page while sign in if they already build burger

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -43,6 +43,12 @@ class Auth extends Component {
         isSignUp: true
     };
 
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     checkValidity(value, rules) {
         let isValid = true;
 
@@ -117,9 +123,8 @@ class Auth extends Component {
         }
 
         let authRedirect = null;
-
         if (this.props.isAuthenticated) {
-            authRedirect = <Redirect to="/"/>
+            authRedirect = <Redirect to={this.props.authRedirectPath}/>
         }
 
         return (
@@ -143,13 +148,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-      onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+      onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+      onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
   }
 };
 
