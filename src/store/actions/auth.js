157aad4996679c174e8bc07803d55a20c2c19bd5
Binod Kafle <mebinod50@gmail.com>
2020-03-20 16:09:06 +0545

    Post sign up data and get token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes'
 
 export const authStart = () => {
@@ -23,5 +24,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA_FjkiXbzAyd0Sdof-EfoRhlHkGw8JQ70', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err))
+            })
     }
 };
