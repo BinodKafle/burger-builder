a0e51cf3d817b9e5051d2ff5c936f92ae9cf4d27
Binod Kafle <mebinod50@gmail.com>
2020-03-20 17:00:01 +0545

    Add spinner and show error message from firebase during sign in and sign up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -36,12 +36,10 @@ export const auth = (email, password, isSignUp) => {
         }
         axios.post(url, authData)
             .then(response => {
-                console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId))
             })
             .catch(err => {
-                console.log(err);
-                dispatch(authFail(err))
+                dispatch(authFail(err.response.data.error))
             })
     }
 };
