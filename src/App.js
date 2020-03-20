1e799bc17f7ec63dec8a5a6fce8dd1aaa5cecfb5
Binod Kafle <mebinod50@gmail.com>
2020-03-20 19:22:38 +0545

    Add persistent auth state with local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -7,9 +8,14 @@ import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
+import * as actions from './store/actions/index';
 
 class App extends Component {
-  render() {
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
+    render() {
     return (
         <div>
             <Layout>
@@ -26,5 +32,11 @@ class App extends Component {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState())
+    }
+};
 
-export default App;
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -50,7 +53,11 @@ export const auth = (email, password, isSignUp) => {
         }
         axios.post(url, authData)
             .then(response => {
-                dispatch(authSuccess(response.data.idToken, response.data.localId))
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
@@ -64,4 +71,22 @@ export const setAuthRedirectPath = (path) => {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
     }
-}
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+          dispatch(logout());
+      } else {
+          const expirationDate = new Date(localStorage.getItem('expirationDate'));
+          if (expirationDate < new Date()) {
+              dispatch(logout())
+          } else {
+              const userId = localStorage.getItem('userId');
+              dispatch(authSuccess(token, userId));
+              dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ))
+          }
+      }
+  }
+};
