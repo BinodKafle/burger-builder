4ba1ec3adc9d52887d012d1fcc0d6dcdb9f0d64b
Binod Kafle <mebinod50@gmail.com>
2020-03-20 17:38:55 +0545

    Update header link to logout if authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,13 @@ import React from "react";
 import classes from './NavigationItems.css';
 import NavigatinItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigatinItem link="/" exact>Burger Builder</NavigatinItem>
         <NavigatinItem link="/orders">Orders</NavigatinItem>
-        <NavigatinItem link="/auth">Authenticate</NavigatinItem>
+        { !props.isAuthenticated
+            ? <NavigatinItem link="/auth">Authenticate</NavigatinItem>
+            : <NavigatinItem link="/logout">Logout</NavigatinItem>}
     </ul>
 );
 
