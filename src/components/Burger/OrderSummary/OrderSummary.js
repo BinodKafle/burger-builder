77184caa5eb251e837bf6f719860c5254c152266
Binod Kafle <mebinod50@gmail.com>
2020-03-23 14:54:28 +0545

    Remove console logs

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -6,10 +6,6 @@ import Button from '../../UI/Button/Button';
 class OrderSummary extends Component {
     // This could be a functional component, doesn't have to be a class
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('Order summary');
-    }
-
     render() {
         const ingredientSummery = Object.keys(this.props.ingredients)
             .map((igKey) => {
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,10 +9,6 @@ class Modal extends Component {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('modal did update')
-    }
-
     render() {
         return (
             <Aux>
